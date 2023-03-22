ada2c18fe2652833d368fe9ef4336ecc70261b45
unknown <fuzzfuzz20@naver.com>
2023-03-22 17:52:11 +0900

    merge

--- components/page/signup/step01.tsx
+++ components/page/signup/step01.tsx
@@ -31,7 +31,7 @@ const Step01 = ({ inputData, setInputData }: ChildProps) => {
                 isAdvertisionConfirm: checked
             }),
             setAddOption({
-                
+
 
             })
         }
